643401379a43ce8ce89b8df7f3d186a378a70691
kenkyou <240605110116@student.uin-malang.ac.id>
2025-03-22 20:52:19 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,7 +5,7 @@ git clone --depth=1 --recursive https://github.com/Tiktodz/android_kernel_asus_s
 rm -rf kernel/asus/sdm660/KernelSU-Next/userspace
 
 rm -rf device/asus/sdm660-common
-git clone --depth=1 https://github.com/ikwfahmi/android_device_asus_sdm660-common-4.19 device/asus/sdm660-common
+git clone https://github.com/electrolaboratory/common -b aosp device/asus/sdm660-common
 
 rm -rf vendor/asus
 git clone --depth=1 https://github.com/electrolaboratory/vendor_common -b v vendor/asus
