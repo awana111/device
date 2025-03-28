8c36f6506f71f1a87518ada904b1807ea336914f
kenkyou <240605110116@student.uin-malang.ac.id>
2025-03-28 17:12:04 +0700

    Update lineage_X00TD.mk

--- lineage_X00TD.mk
+++ lineage_X00TD.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2020-2024 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -7,17 +7,20 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common LineageOS stuff
+# Inherit some common ProjectBlaze stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
-# Inherit from X00TD device
-$(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 TARGET_DISABLE_EPPE := true
 RISING_MAINTAINER := Kyura
+WITH_GMS := false
 TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
+
+# Boot animaton
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Inherit from X00TD device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_X00TD
@@ -30,7 +33,5 @@ PRODUCT_GMS_CLIENTID_BASE := android-asus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     DeviceProduct=ASUS_X00TD \
-    BuildDesc="sdm660_64-user 10 QKQ1 72 release-keys" \
-    BuildFingerprint=asus/ASUS_X00TD/ASUS_X00TD:10/QKQ1/17.2017.2012.438-20201203:user/release-keys \
-    RisingChipset="Snapdragon™_636" \
+    RisingChipset="Snapdragon 636" \
     RisingMaintainer="Kyura"
