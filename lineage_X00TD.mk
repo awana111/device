72a8021914080e1e70d34c0f0551145d451badd3
kenkyou <240605110116@student.uin-malang.ac.id>
2025-03-30 04:36:58 +0700

    Update lineage_X00TD.mk

--- lineage_X00TD.mk
+++ lineage_X00TD.mk
@@ -16,11 +16,11 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 TARGET_DISABLE_EPPE := true
-TARGET_INCLUDE_VIPERFX := true
-AXION_CAMERA_REAR_INFO := 13,16,5
-AXION_CAMERA_FRONT_INFO := 16
-AXION_MAINTAINER := Kyura
-AXION_PROCESSOR := Qualcomm®_Snapdragon™_636
+MISTOS_MAINTAINER="XXX"
+TARGET_ENABLE_BLUR := false
+PRODUCT_NO_CAMERA := false
+TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := false 
+WITH_GMS := false
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_X00TD
