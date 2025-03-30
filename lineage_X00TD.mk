ba6f19175b8e4c6d32ac70ec06280a9d31957521
kenkyou <240605110116@student.uin-malang.ac.id>
2025-03-30 14:56:21 +0700

    Update lineage_X00TD.mk

--- lineage_X00TD.mk
+++ lineage_X00TD.mk
@@ -16,9 +16,6 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 TARGET_DISABLE_EPPE := true
-TARGET_ENABLE_BLUR := false
-PRODUCT_NO_CAMERA := false
-TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := false
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_X00TD
