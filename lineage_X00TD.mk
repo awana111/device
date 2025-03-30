6a578b0cd8b7e2299703470f7d093dfdd83852d0
kenkyou <240605110116@student.uin-malang.ac.id>
2025-03-30 15:48:04 +0700

    Update lineage_X00TD.mk

--- lineage_X00TD.mk
+++ lineage_X00TD.mk
@@ -16,11 +16,11 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 TARGET_DISABLE_EPPE := true
-TARGET_INCLUDE_VIPERFX := true
-AXION_CAMERA_REAR_INFO := 13,16,5
-AXION_CAMERA_FRONT_INFO := 16
-AXION_MAINTAINER := Kyura
-AXION_PROCESSOR := Qualcomm®_Snapdragon™_636
+ORION_MAINTAINER := Kyura
+ORION_MAINTAINER_LINK := https://t.me/KyuraProject
+ORION_BUILD_TYPE := Unofficial
+ORION_GAPPS := false
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_X00TD
