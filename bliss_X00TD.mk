92642e3a5e6fe501743b9d5f2fa0241cfb42ef3e
kenkyou <240605110116@student.uin-malang.ac.id>
2025-04-04 20:04:43 +0700

    Update bliss_X00TD.mk

--- bliss_X00TD.mk
+++ bliss_X00TD.mk
@@ -10,7 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit some common LineageOS stuff
-$(call inherit-product, vendor/pixelage/config/common_full_phone.mk)
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
