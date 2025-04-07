e18681e207e2ce91ab6d943f93c6eb463da17000
kenkyou <240605110116@student.uin-malang.ac.id>
2025-04-07 10:08:04 +0700

    Update lineage_X00TD.mk

--- lineage_X00TD.mk
+++ lineage_X00TD.mk
@@ -12,9 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 TARGET_DISABLE_EPPE := true
-RISING_MAINTAINER := Kyura
 WITH_GMS := false
-TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
 
 # Boot animaton
 TARGET_BOOT_ANIMATION_RES := 1080
