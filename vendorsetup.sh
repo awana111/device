e35b5e23455003a275939125a5611c907b2c064d
awana111 <80144987+awana111@users.noreply.github.com>
2025-04-12 05:41:28 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 rm -rf kernel/asus/sdm660
 git clone --depth=1 --recursive https://github.com/Tiktodz/android_kernel_asus_sdm660 -b master kernel/asus/sdm660
 rm -rf kernel/asus/sdm660/KernelSU-Next/userspace
