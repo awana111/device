1e54c2c9b8641aaed1e18a415652cd6a98e83643
awana111 <80144987+awana111@users.noreply.github.com>
2025-04-12 05:38:08 +0700

    Update and rename pixelage_X00TD.mk to halcyon_X00TD.mk

--- halcyon_X00TD.mk
+++ halcyon_X00TD.mk
@@ -7,11 +7,9 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit some common LineageOS stuff
-$(call inherit-product, vendor/pixelage/config/common_full_phone.mk)
-
+$(call inherit-product, vendor/halcyon/config/common.mk)
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
@@ -19,7 +17,7 @@ TARGET_DISABLE_EPPE := true
 PIXELAGE_MAINTAINER := Kyura
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := pixelage_X00TD
+PRODUCT_NAME := halcyon_X00TD
 PRODUCT_DEVICE := X00TD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone Max Pro M1
