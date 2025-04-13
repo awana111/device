94a56ee7c5f0ce11a0968069e70b6570da7f4539
awana111 <80144987+awana111@users.noreply.github.com>
2025-04-13 15:12:59 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,7 +3,7 @@ git clone --depth=1 --recursive https://github.com/Tiktodz/android_kernel_asus_s
 rm -rf kernel/asus/sdm660/KernelSU-Next/userspace
 
 rm -rf device/asus/sdm660-common
-git clone --depth=1 https://github.com/awana111/android_device_asus_sdm660-common-4.19 -b 15.0 device/asus/sdm660-common
+git clone --depth=1 https://github.com/awana111/android_device_asus_sdm660-common-4.19 device/asus/sdm660-common
 
 rm -rf vendor/asus
 git clone --depth=1 https://github.com/electrolaboratory/vendor_common -b v vendor/asus
