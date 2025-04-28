e97599b1485c4e135934f3da4ddac707747085a7
awana <80144987+awana111@users.noreply.github.com>
2025-04-28 15:59:12 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,20 +1,19 @@
+#!/bin/bash
+
 rm -rf kernel/asus/sdm660
-git clone --depth=1 --recursive https://github.com/Tiktodz/android_kernel_asus_sdm660 -b wip kernel/asus/sdm660
+git clone --depth=1 --recursive https://github.com/Tiktodz/android_kernel_asus_sdm660 -b  stable kernel/asus/sdm660
 rm -rf kernel/asus/sdm660/KernelSU-Next/userspace
-rm -rf kernel/asus/sdm660/KernelSU-Next/website
-rm -rf kernel/asus/sdm660/KernelSU-Next/scripts
-rm -rf kernel/asus/sdm660/KernelSU-Next/docs
+
+sed -i 's/CONFIG_DEBUG_INFO=.*/CONFIG_DEBUG_INFO=n/g' kernel/asus/sdm660/arch/arm64/configs/asus/X00TD_defconfig
 
 rm -rf device/asus/sdm660-common
-git clone --depth=1 https://github.com/ikwfahmi/common -b rising device/asus/sdm660-common
+git clone --depth=1 https://github.com/ikwfahmi/android_device_asus_sdm660-common-4.19 -b 15.0 device/asus/sdm660-common
 
 rm -rf vendor/asus
 git clone --depth=1 https://github.com/electrolaboratory/vendor_common -b v vendor/asus
 
 rm -rf vendor/lineage-priv/keys
-rm -rf vendor/lineage/signing/keys
-mkdir -p vendor/lineage-priv/keys
-git clone https://github.com/electrolaboratory/public-keys vendor/lineage-priv/keys/
+git clone https://github.com/sotodrom/keys vendor/lineage-priv/keys
 
 export KBUILD_BUILD_USER=kenkyou
 export BUILD_USER=kenkyou
